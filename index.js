688c146874d19485a0a6fb0742639b963b3457d0
Ephy Muiruri Wachira <ephywachira89@gmail.com>
2024-03-27 16:31:38 +0300

    find lab complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+  const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+  function superbowlWin(win){
+        for (let element of win){
+            if (element.result ==="W"){
+                console.log(element.result)
+                return element.year
+            }
+        }
+        return undefined
+    }
+console.log(record.find(superbowlWin))
